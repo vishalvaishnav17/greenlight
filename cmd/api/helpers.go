fb7495e6636d136e8f76db79fe88fd9e591f22e4
Vishal Vaishnav <vishalvaishnav1007@icloud.com>
2024-12-12 23:42:49 +0530

    Checkpoint chapter 3.5

---
+++ cmd/api/helpers.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+type envelope map[string]interface{}
+
+func readIDParam(r *http.Request) (int64, error) {
+	params := httprouter.ParamsFromContext(r.Context())
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	if err != nil {
+		return 0, errors.New("Invalid id parameter")
+	}
+	return id, nil
+}
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
+	js, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
+	for header, value := range headers {
+		w.Header()[header] = value
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+	return nil
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module greenlight.vishalvaishnav.net
 
 go 1.23.4
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
